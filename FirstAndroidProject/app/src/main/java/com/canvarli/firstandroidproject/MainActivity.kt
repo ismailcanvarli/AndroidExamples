7bc74c1e6800c793aa94cdd244dbdb44f90d0351
İsmail Can Varlı <icanvarli@outlook.com>
2023-03-28 22:31:23 +0300

    İlk proje yapıldı. Butona ile isim değiştirme

--- FirstAndroidProject/app/src/main/java/com/canvarli/firstandroidproject/MainActivity.kt
+++ FirstAndroidProject/app/src/main/java/com/canvarli/firstandroidproject/MainActivity.kt
@@ -2,10 +2,20 @@ package com.canvarli.firstandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+//burada butona tıkladığında isim değiştirme işlemini yapacağız.
+//view adında View tipinde bir değişken oluşturduk.
+    fun changeName(view : View){
+        textView.text = "Merhaba Android";
+    }
+
 }
