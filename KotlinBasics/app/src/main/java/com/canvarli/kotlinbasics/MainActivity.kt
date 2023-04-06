cc252f3b753bb7c4037aa0de3eea277c2a8fec7c
İsmail Can Varlı <icanvarli@outlook.com>
2023-04-06 23:47:27 +0300

    if kontrolleri, when ve  döngüler işlendi

--- KotlinBasics/app/src/main/java/com/canvarli/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/canvarli/kotlinbasics/MainActivity.kt
@@ -22,9 +22,9 @@ class MainActivity : AppCompatActivity() {
         val c = 10
 
         //değer tanımlama ve değer atama.
-        val sayi : Int
-        sayi = 7
-        println(sayi)
+        val sayi1 : Int
+        sayi1 = 7
+        println(sayi1)
 
 
         println("----------------------- Long ------------------------")
@@ -115,5 +115,121 @@ class MainActivity : AppCompatActivity() {
 
         println(foodCaloryMap.get("Et"))
 
+
+        //Matematiksel İşlemler
+        println("----------Matematiksel İşlemler---------")
+
+        var sayi = 8
+        println(sayi)
+        sayi = sayi + 1
+        println(sayi)
+        sayi++
+        println(sayi)
+        sayi--
+        println(sayi)
+
+        var digerSayi = 10
+
+        println(digerSayi > sayi)
+
+        println(digerSayi > sayi && 2 > 3)  //VE
+        println(digerSayi > sayi || 2 > 3)  //VEYA
+
+        println(8+7)
+        println(10-4)
+        println(20*5)
+        println(10/2)
+
+        //Remainder - Kalanı Bulmak
+        println(11%3)
+
+        //If Kontrolleri
+        println("----------If Statements (Eğer Kontrolleri)---------")
+
+        val skor = 80
+
+        if (skor < 10) {
+            println("Oyunu kaybettin!")
+        } else if (skor >= 10 && skor < 20) {
+            println("Skorun 10 ve 20 arasında, çok iyi skor aldın")
+        } else if (skor >= 20 && skor < 30) {
+            println("Skorun 20 ve 30 arasında, rekorlar kırıyorsun")
+        } else {
+            println("Skorun 30'un üstünde, efsane oynadın")
+        }
+
+        //When - Switch case oluyor java da
+        println("----------When---------")
+
+        var notRakami = 2
+        var notStringi = ""
+
+
+        when(notRakami) {
+            0 -> notStringi = "Geçersiz Not"
+            1 -> notStringi = "Zayıf"
+            2 -> notStringi = "Kötü"
+            3 -> notStringi = "Orta"
+            4 -> notStringi = "İyi"
+            else -> notStringi = "Pek İyi"
+        }
+        println(notStringi)
+
+        /*
+        if (notRakami == 0) {
+            notStringi = "Geçersiz Not"
+        } else if (notRakami == 1) {
+            notStringi = "Zayıf"
+        } else if (notRakami == 2) {
+            notStringi = "Kötü"
+        } else if (notRakami == 3) {
+            notStringi = "Orta"
+        } else if (notRakami == 4) {
+            notStringi = "İyi"
+        } else {
+            notStringi = "Pek İyi"
+        }
+*/
+
+
+        //Döngüler
+        println("----------For Döngüsü---------")
+
+        val baskaBirDizi = arrayOf(5,10,15,20,25,30)
+
+        println("döngü başladı")
+        for (num in baskaBirDizi) {
+            println(num / 5 + 3)
+        }
+        println("döngü bitti")
+        
+        for (b in 0..9) {
+            println(b)
+        }
+
+        val benimDigerStringDizim = ArrayList<String>()
+        benimDigerStringDizim.add("İsmail Can")
+        benimDigerStringDizim.add("VARLI")
+
+        for (str in benimDigerStringDizim) {
+            println(str)
+        }
+
+        benimDigerStringDizim.forEach {
+            println(it)
+        }
+
+        //While
+        println("----------While Döngüsü---------")
+
+        var j = 0
+
+        while (j <= 10) {
+            println(j)
+            j = j + 1
+        }
+
     }
+
+
 }
