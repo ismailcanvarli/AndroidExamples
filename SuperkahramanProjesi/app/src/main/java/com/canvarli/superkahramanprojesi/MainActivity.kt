2ea7a4ffe99bb98a461f2c4e83b505caa54311ef
İsmail Can Varlı <icanvarli@outlook.com>
2023-04-10 19:07:41 +0300

    Süper kahraman projesi (Girdi alıp değer döndür)

--- SuperkahramanProjesi/app/src/main/java/com/canvarli/superkahramanprojesi/MainActivity.kt
+++ SuperkahramanProjesi/app/src/main/java/com/canvarli/superkahramanprojesi/MainActivity.kt
@@ -2,10 +2,41 @@ package com.canvarli.superkahramanprojesi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var numara = 0
+    private var isim = ""
+    private var yas : Int? = null
+    private var meslek = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Scope - Kapsam
+        numara = 5
+
+    }
+
+    fun superKahramanYap(view : View){
+        numara = 10
+
+        //İnput kısmında yazılanları alıyoruz ve onları istenilen veri tipine dönüştürüp
+        //işlemimize devam ediyoruz.
+        isim = isimText.text.toString()
+        yas = yasText.text.toString().toIntOrNull() //İnt'e çeviremezse null olarak yazacak.
+        meslek = meslekText.text.toString()
+
+
+        if (yas == null) {
+            sonucText.text = "Doğru Yaşı Giriniz"
+        } else {
+            val superkahraman = SuperKahraman(isim,yas!!,meslek)
+
+            sonucText.text = "İsim: ${superkahraman.isim} Yaş: ${superkahraman.yas} " +
+                                "Meslek: ${superkahraman.meslek}"
+        }
+
     }
 }
---
+++ SuperkahramanProjesi/app/src/main/java/com/canvarli/superkahramanprojesi/SuperKahraman.kt
@@ -0,0 +1,5 @@
+package com.canvarli.superkahramanprojesi
+
+class SuperKahraman(var isim: String, var yas: Int, var meslek: String) {
+
+}
