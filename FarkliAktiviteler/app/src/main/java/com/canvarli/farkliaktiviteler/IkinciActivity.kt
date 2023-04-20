37b9f37db06ac0305d2ee94f5153b9fa0878738e
İsmail Can Varlı <icanvarli@outlook.com>
2023-04-21 00:44:04 +0300

    Aktiviteler arası intent ile veri yollandı

--- FarkliAktiviteler/app/src/main/java/com/canvarli/farkliaktiviteler/IkinciActivity.kt
+++ FarkliAktiviteler/app/src/main/java/com/canvarli/farkliaktiviteler/IkinciActivity.kt
@@ -3,11 +3,19 @@ package com.canvarli.farkliaktiviteler
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_ikinci.*
 
 class IkinciActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ikinci)
+
+        //Burada da oradan putExtra fonksiyonu ile verilen veriyi almak için
+        //GetExtra fonksiyonunu kullanıyoruz ve oradan verilen veriyi alıyoruz.
+        val intent = intent
+        val alinanVeri = intent.getStringExtra("yollananVeri")
+        textView2.text = alinanVeri
+
     }
 
 
--- FarkliAktiviteler/app/src/main/java/com/canvarli/farkliaktiviteler/MainActivity.kt
+++ FarkliAktiviteler/app/src/main/java/com/canvarli/farkliaktiviteler/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +14,19 @@ class MainActivity : AppCompatActivity() {
 
     fun aktiviteDegistir(view : View){
         //Bu intent şuraya git anlamına geliyor.
+
+        val kullaniciVerisi = editText.text.toString()
+
         //Application context kısmına sonra değineceğiz.
         //, deyip gitmek istedğimiz sınıfın adını yazıyoruz.
         //sınıfın adını yazarkende böyle yazıyoruz.
         //Hangi sınıfa gideceğimizi veriyoruz hangi context'ten çalışacağımızı veriyoruz.
         val intent = Intent(applicationContext,IkinciActivity::class.java)
+
+        //kullanıcıdan aldığımız veriyi intent ile putExtra fonksiyonu ile
+        // diğer aktiviteye gönderebiliyoruz.
+
+        intent.putExtra("yollananVeri",kullaniciVerisi)
         startActivity(intent)
 
     }
