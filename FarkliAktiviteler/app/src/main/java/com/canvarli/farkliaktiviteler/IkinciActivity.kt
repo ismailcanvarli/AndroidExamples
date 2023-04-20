19e3bcae6cf0a9bdb9d88e2a9362828ea9d670ec
İsmail Can Varlı <icanvarli@outlook.com>
2023-04-21 00:25:19 +0300

    Aktiviteler arası ilk geçiş yapıldı

---
+++ FarkliAktiviteler/app/src/main/java/com/canvarli/farkliaktiviteler/IkinciActivity.kt
@@ -0,0 +1,14 @@
+package com.canvarli.farkliaktiviteler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class IkinciActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ikinci)
+    }
+
+
+}
--- FarkliAktiviteler/app/src/main/java/com/canvarli/farkliaktiviteler/MainActivity.kt
+++ FarkliAktiviteler/app/src/main/java/com/canvarli/farkliaktiviteler/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.canvarli.farkliaktiviteler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun aktiviteDegistir(view : View){
+        //Bu intent şuraya git anlamına geliyor.
+        //Application context kısmına sonra değineceğiz.
+        //, deyip gitmek istedğimiz sınıfın adını yazıyoruz.
+        //sınıfın adını yazarkende böyle yazıyoruz.
+        //Hangi sınıfa gideceğimizi veriyoruz hangi context'ten çalışacağımızı veriyoruz.
+        val intent = Intent(applicationContext,IkinciActivity::class.java)
+        startActivity(intent)
+
+    }
+
 }
