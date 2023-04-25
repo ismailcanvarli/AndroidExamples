768956954b599b5858e0a44ffd53946d53963111
İsmail Can Varlı <icanvarli@outlook.com>
2023-04-25 22:37:49 +0300

    CountDownTimer abstract class'ı ile sayaç yapıldı

--- SayacProjesi/app/src/main/java/com/canvarli/sayacprojesi/MainActivity.kt
+++ SayacProjesi/app/src/main/java/com/canvarli/sayacprojesi/MainActivity.kt
@@ -2,10 +2,29 @@ package com.canvarli.sayacprojesi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Abstract Class (soyut sınıf)
+        //soyut sınıftan nesne oluşturulamaz. Direk uygulanır.
+        //Abstract classlarda o sınıfta bulunan fonksiyonları kullanmak zorundayız.
+        //bu countDownTimer hazır bir fonksiyon ve zaten bu işlemi yapıyor.
+        object : CountDownTimer(15000,1000){
+
+            //sayaç bittiğinde ne yapsın diyor
+            override fun onFinish() {
+                textView.text = "Kalan: 0"
+            }
+
+            //her bir tik attığında ne yapılsın diyor. p0 kalan milisaniyedir.
+            override fun onTick(p0: Long) {
+                textView.text = "Kalan: ${p0 / 1000}"
+            }
+        }.start() // start'ı yazmazsak çalışmaz.
     }
 }
