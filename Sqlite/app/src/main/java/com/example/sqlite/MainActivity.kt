1621fb7d2021976b0b4dcdcb1159eda815ff495d
canMac <icanvarli@outlook.com>
2023-11-25 23:38:17 +0300

    Database: data read and write operations

--- Sqlite/app/src/main/java/com/example/sqlite/MainActivity.kt
+++ Sqlite/app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -17,9 +17,26 @@ class MainActivity : AppCompatActivity() {
             val database = this.openOrCreateDatabase("Products", MODE_PRIVATE, null)
             //sorguyu çalıştır diyoruz. Eğer yoksa oluştur dedik. Tablo ismini verdik.
             database.execSQL(
-                "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, " +
-                        "name VARCHAR, price INT)"
+                "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, " + "name VARCHAR, price INT)"
             )
+            //database.execSQL("INSERT INTO products (name, price) VALUES ('Shoes',100)")
+
+            //burada bir imleç oluşturduk ve veritabanına kaydettiğimiz verileri alma işlemi yaptık.
+            //imleç tek tek geziyor ve okuduğu verileri bize yazıyor.
+            val cursor = database.rawQuery("SELECT * FROM products", null)
+
+            //burada da oluşturduğumuz kolonlar için kolon index kısmı oluşturduk.
+            val idColumnIndex = cursor.getColumnIndex("id")
+            val nameColumnIndex = cursor.getColumnIndex("name")
+            val priceColumnIndex = cursor.getColumnIndex("price")
+
+            while (cursor.moveToNext()) {
+                println("Id: ${cursor.getBlob(idColumnIndex)}")
+                println("Name: ${cursor.getString(nameColumnIndex)}")
+                println("Price: ${cursor.getInt(priceColumnIndex)}")
+            }
+
+            cursor.close()
         } catch (e: Exception) {
             e.printStackTrace()  //bu hatanın log kaydını yazdırıyor.
         }
