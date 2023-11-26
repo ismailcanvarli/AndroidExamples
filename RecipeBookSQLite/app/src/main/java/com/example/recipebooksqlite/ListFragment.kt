96d79915a7b934846e3ac70f60980f1974b22f7b
canMac <icanvarli@outlook.com>
2023-11-27 00:06:26 +0300

    Created Recipe fragment xml file.

---
+++ RecipeBookSQLite/app/src/main/java/com/example/recipebooksqlite/ListFragment.kt
@@ -0,0 +1,17 @@
+package com.example.recipebooksqlite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+
---
+++ RecipeBookSQLite/app/src/main/java/com/example/recipebooksqlite/RecipeFragment.kt
@@ -0,0 +1,17 @@
+package com.example.recipebooksqlite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RecipeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
