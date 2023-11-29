9c1fa6a9ea67444c33a0ab6ee35393ac9a58188b
canMac <icanvarli@outlook.com>
2023-11-29 21:47:31 +0300

    Convert the selected image to a bitmap.

--- RecipeBookSQLite/app/src/main/java/com/example/recipebooksqlite/RecipeFragment.kt
+++ RecipeBookSQLite/app/src/main/java/com/example/recipebooksqlite/RecipeFragment.kt
@@ -1,18 +1,23 @@
 package com.example.recipebooksqlite
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 
 class RecipeFragment : Fragment() {
+    var chosenPicture: Uri? = null //Seçilen görselin adresini tutucaz.
+    var chosenBitmap : Bitmap? = null //görseli dönüştürmek için
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +26,6 @@ class RecipeFragment : Fragment() {
     //Fragmentlarda eğerki butona bir fonksiyon atıyorsak bunu setOnClick listener ile yapmalıyız
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val saveButton = view.findViewById<Button>(R.id.saveButton)
-        val imageView = view.findViewById<ImageView>(R.id.imageView)
 
         saveButton.setOnClickListener {
             savePicture(it)
@@ -48,8 +50,7 @@ class RecipeFragment : Fragment() {
             //manifest'Te okuma iznimiz olup olmadığını kontrol ettik.
             //PackageManager'da ise bunun izin verildiye eşit değilse yani izin verilmediyse
             if (ContextCompat.checkSelfPermission(
-                    it.applicationContext,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
+                    it.applicationContext, Manifest.permission.READ_EXTERNAL_STORAGE
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
                 //izin verilmedi izin istenmesi gerekiyor.
@@ -67,6 +68,56 @@ class RecipeFragment : Fragment() {
             }
         }
 
+    }
+
+    //istenilen izinlerin sonuçlarını değerlendirdiğimiz fonksiyon
+    override fun onRequestPermissionsResult(
+        requestCode: Int,      //istek kodu belirlemiştik 1 ve 2 diye
+        permissions: Array<out String>,  //izin çeşidi
+        grantResults: IntArray       //verilen sonuçlar
+    ) {
+
+        if (requestCode == 1) {
+            //izni aldığımız koşul tam olarka burası oluyor.
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent, 2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        //verinin boş olup olmadığına baktık. RequestCode'un 2 olup olmadığına baktık
+        //Activity'nin durumunun tamam mı olduğuna baktık. Belki olumsuz olabilir.
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            //artık seçilen görselin uri'na yani yoluna sahibiz. Bu yolu aldık.
+            chosenPicture = data.data
+
+            //görseli o konumdan alıcaz bitmap'e çevirip kaydedeceğiz.
+            //Bu işlemi try catch ile yapıyoruz. Hatalarla karşılaşabiliriz.
+            //Bu işlemi daha önce kullandığımız bitmap factory tarzı bir yapıyla yapıcaz.
+            try {
+                context?.let {
+                    if(chosenPicture != null) {
+                        if (Build.VERSION.SDK_INT >= 28){
+                            //create soruce fonksiyonu api 28'den sonra çalışıyor.
+                            val source = ImageDecoder.createSource(it.contentResolver, chosenPicture!!)
+                            chosenBitmap = ImageDecoder.decodeBitmap(source)
+                            imageView.setImageBitmap(chosenBitmap)
+                        } else {
+                            chosenBitmap = MediaStore.Images.Media.getBitmap(it.contentResolver, chosenPicture)
+                            imageView.setImageBitmap(chosenBitmap)
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 }
